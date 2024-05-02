c41fdc1191d600c575b3549e61f9979ce4bf9de4
pallavisharma-13 <131350293+pallavisharma-13@users.noreply.github.com>
2024-05-02 15:32:10 +0530

    Initial commit
    Adding Service Framework

---
+++ Service/Service/APIManager.swift
@@ -0,0 +1,37 @@
+//
+//  APIManager.swift
+//  Service
+//
+//  Created by Pallavi on 02/05/24.
+//
+
+import Foundation
+ 
+class APIManager {
+    static let shared = APIManager()
+    
+    private init() {}
+    
+    func request<T: Codable>(endpoint: String, method: String, parameters: [String: Any]?, completion: @escaping (Result<T, Error>) -> Void) {
+        // Perform the API request with the provided endpoint, method, and parameters
+        let url = URL(string: endpoint)
+        // Assuming you're using URLSession to make the API request
+        URLSession.shared.dataTask(with: url!) { (data, response, error) in
+            // Handle the API response
+            // Assuming you have a generic JSONDecoder to decode the API response
+            let decoder = JSONDecoder()
+            // Decode the API response data
+            if let data = data {
+                do {
+                    let decodedResponse = try decoder.decode(T.self, from: data)
+                    completion(.success(decodedResponse))
+                } catch {
+                    completion(.failure(error))
+                }
+            } else if let error = error {
+                completion(.failure(error))
+            }
+            
+        }.resume()
+    }
+}
